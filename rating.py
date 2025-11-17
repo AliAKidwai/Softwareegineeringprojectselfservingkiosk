0cad492cc3317e4bf8c4cc36dc1d34259b45daaa
AliAKidwai <alikidwaitx@gmail.com>
2025-11-17 09:44:48 -0600

    Update rating.py

--- rating.py
+++ rating.py
@@ -1,1 +1,14 @@
+#input rating
+rating = int(input("Thank you for getting coffee with us on a scale of 1 to 5 how would you rate your experience: "))
+print("Enter your ratings : 1 to 5")
+if rating == 1:
+    print("Apologies for the inconvience.")
+elif rating == 2:
+     print("We will try to do better next time!")
+elif rating == 3:
+     print("Thank you!")
+elif rating == 4:
+     print("We are glad you enjoyed in our cafe!")
+elif rating == 5:
+     print("Awesome, hope you have wonderful day!")
 
